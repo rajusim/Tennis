7d01342b1f2f56c1e46daa526796dbac65f4833b
rajusim <rajusim@gmail.com>
2024-01-29 12:25:55 +0000

    Initial commit

---
+++ Tennis.Game/TennisGame.cs
@@ -0,0 +1,44 @@
+﻿namespace Tennis.Game
+{
+    public class TennisGame : ITennisGame
+    {
+        private int m_score1 = 0;
+        private int m_score2 = 0;
+        private string player1Name;
+        private string player2Name;
+
+        public TennisGame(string player1Name, string player2Name)
+        {
+            this.player1Name = player1Name;
+            this.player2Name = player2Name;
+        }
+
+        public void WonPoint(string playerName)
+        {
+            if (playerName == "player1")
+                m_score1 += 1;
+            else
+                m_score2 += 1;
+        }
+
+        public string GetScore()
+        {
+            string score = "";
+            if (m_score1 == m_score2)
+            {
+                score = TennisGameStrings.EqualScoreEnum[m_score1];
+            }
+            else if (m_score1 >= 4 || m_score2 >= 4)
+            {
+                score = TennisGameStrings.ScoreWinEnum[m_score1 - m_score2];
+            }
+            else
+            {
+                for (var i = 1; i < 3; i++)
+                    score = TennisGameStrings.ScoreEnum[string.Format("{0}{1}", m_score1, m_score2)];
+            }
+            return score;
+        }
+       
+    }
+}
---
+++ Tennis.Game/TennisGameStrings.cs
@@ -0,0 +1,60 @@
+﻿namespace Tennis.Game
+{
+    public static class TennisGameStrings
+    {
+        public static Dictionary<int, string> EqualScoreEnum
+        {
+            get
+            {
+                var dict = new Dictionary<int, string>();
+                dict.Add(0, "Love-All");
+                dict.Add(1, "Fifteen-All");
+                dict.Add(2, "Thirty-All");
+                dict.Add(3, "Deuce");
+                dict.Add(4, "Deuce");
+                return dict;
+            }
+        }
+        public static Dictionary<int, string> ScoreWinEnum
+        {
+            get
+            {
+                var dict = new Dictionary<int, string>();
+                dict.Add(-1, "Advantage player2");
+                dict.Add(1, "Advantage player1");
+                dict.Add(2, "Win for player1");
+                dict.Add(-2, "Win for player2");
+                dict.Add(3, "Win for player1");
+                dict.Add(-3, "Win for player2");
+                dict.Add(4, "Win for player1");
+                dict.Add(-4, "Win for player2");
+                return dict;
+            }
+        }
+
+        public static Dictionary<string, string> ScoreEnum
+        {
+            get
+            {
+                var dict = new Dictionary<string, string>();
+                dict.Add("00", "Love");
+                dict.Add("01", "Love-Fifteen");
+                dict.Add("02", "Love-Thirty");
+                dict.Add("03", "Love-Forty");
+                dict.Add("10", "Fifteen-Love");
+                dict.Add("12", "Fifteen-Thirty");
+                dict.Add("13", "Fifteen-Forty");
+                dict.Add("20", "Thirty-Love");
+                dict.Add("21", "Thirty-Fifteen");
+                dict.Add("23", "Thirty-Forty");
+                dict.Add("30", "Forty-Love");
+                dict.Add("31", "Forty-Fifteen");
+                dict.Add("32", "Forty-Thirty");
+                return dict;
+            }
+        }
+
+    }
+}
+
+
